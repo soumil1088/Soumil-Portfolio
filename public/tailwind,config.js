bac855a9dd73b7e91818b680756fd16976c1716e
soumil1088 <soumil.mukherjee88@gmail.com>
2025-09-28 00:36:01 -0500

    Create tailwind,config.js

---
+++ public/tailwind,config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    }
+  },
+  plugins: []
+};
